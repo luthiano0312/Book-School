69671f9f1c4c33eab66bd863a848b4786b109a5a
luthiano0312 <luthiano.penha@aluno.ce.gov.br>
2025-05-27 22:08:35 -0300

    Delete login/connection.php

--- login/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-    try {
-        $conn = new PDO("mysql:host=localhost;dbname=bookschool;charset=utf8mb4","root","");
-    } catch (PDOException $e) {
-        echo "erro: " . $e->getmessage();
-    }
-?>
