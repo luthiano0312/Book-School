df85aab740281bc1a1845b9d5ecf1cd94f95545c
luthiano0312 <luthiano.penha@aluno.ce.gov.br>
2025-06-11 11:32:59 -0300

    formulario do update e update de aluno

---
+++ CRUD_aluno/form_update_aluno.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="create-aluno.php" method="post">
+
+        <h1>cadastre-se</h1>
+
+        <input type="hidden" name="nome" id="nome" value="<?php echo $_POST["id_aluno"];?>">
+
+        <label for="nome">Nome: </label>
+        <input type="text" name="nome" id="nome" value="<?php echo $_POST["nome"];?>">
+
+        <br> <br>   
+
+        <label for="email">Email: </label>
+        <input type="email" name="email" id="email" value="<?php echo $_POST["email"];?>">
+
+        <br> <br>
+        <!-- precisa terminar -->
+        <label for="senha">Senha: </label>
+        <input type="password" name="senha" id="senha" value="<?php echo $_POST["id_aluno"];?>">
+
+        <br> <br>
+
+        <input type="submit" value="cadastrar-se">
+    </form>
+</body>
+</html>
