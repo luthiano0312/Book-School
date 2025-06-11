65f4531eb11ae6d4fd594c3cec1b80d7d2f0a25c
luthiano0312 <luthiano.penha@aluno.ce.gov.br>
2025-06-11 11:25:36 -0300

    organizado na pasta login

---
+++ login/authenticate.php
@@ -0,0 +1,32 @@
+<?php
+    require "../connection.php";
+
+    session_start();
+
+    $email = $_POST["email"] ?? "";
+    $senha = $_POST["senha"] ?? "";
+    
+    if (!$senha || !$email) {
+        $_SESSION["erro"] = "Prencha os campos";
+        header("location: login.php");
+        exit;
+    }
+
+    $stmt = $conn->prepare("SELECT * FROM bibliotecarios WHERE EMAIL = :email;");
+    $stmt->bindValue(":email", $email);
+    $stmt->execute();
+    $usuario = $stmt->fetch();
+    
+    if ($usuario && password_verify($senha, $usuario["senha"])) {
+        $_SESSION["id_usuario"] = $usuario["id_bibliotecario"];
+        $_SESSION["id_escola"] = $usuario["id_escola"];
+        header("location: ../CRUD_aluno/dashboard.php");
+        exit;
+    } else {
+        $_SESSION["erro"] = "Senha ou email incorretos";
+        header("location: login.php");
+        exit;
+    }
+
+?>
+<!-- cuidado luthiano vou tomar cuidado -->
