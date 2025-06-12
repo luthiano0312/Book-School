680469a56e39e125cf768bc017c64d7a137096fa
luthiano0312 <luthiano.penha@aluno.ce.gov.br>
2025-06-12 20:49:54 -0300

    corrigido bug que exibia dados de outras escolas no read

--- CRUD_aluno/dashboard.php
+++ CRUD_aluno/dashboard.php
@@ -22,36 +22,39 @@
 
             require_once "../connection.php";
 
-            $stmt = $conn->prepare("SELECT * FROM alunos;");
-            $stmt->execute();
+            $id_escola = $_SESSION["id_escola"];
 
-            while ($rows = $stmt->fetch(PDO::FETCH_OBJ)) { ?>
-                <tr>
-                    <td><?php echo $rows->id_aluno; ?></td>
-                    <td><?php echo $rows->id_escola; ?></td>
-                    <td><?php echo $rows->nome; ?></td>
-                    <td><?php echo $rows->email; ?></td>
-                    <td>
-                        <form action="form_update_aluno.php" method="post">
-                            <input type="hidden" name="id_aluno" value="<?php echo $rows->id_aluno; ?>">
-                            <input type="hidden" name="nome" value="<?php echo $rows->nome; ?>">
-                            <input type="hidden" name="email" value="<?php echo $rows->email; ?>">
-                            <input type="submit" value="editar">
-                        </form>
-                    </td>
-                    <td>
-                        <form action="delete.php" method="post">
-                            <input type="hidden" name="id_aluno" value="<?php echo $rows->id_aluno; ?>">
-                            <input type="submit" value="excluir">
-                        </form>
-                    </td>
-                </tr>
-            <?php } ?>
+            $stmt = $conn->prepare("SELECT * FROM alunos WHERE id_escola = :id;");
+            $stmt->bindValue(":id", $id_escola);
+            if ($stmt->execute()) {
+                while ($rows = $stmt->fetch(PDO::FETCH_OBJ)) { ?>
+                    <tr>
+                        <td><?php echo $rows->id_aluno; ?></td>
+                        <td><?php echo $rows->id_escola; ?></td>
+                        <td><?php echo $rows->nome; ?></td>
+                        <td><?php echo $rows->email; ?></td>
+                        <td>
+                            <form action="form_update_aluno.php" method="post">
+                                <input type="hidden" name="id_aluno" value="<?php echo $rows->id_aluno; ?>">
+                                <input type="hidden" name="nome" value="<?php echo $rows->nome; ?>">
+                                <input type="hidden" name="email" value="<?php echo $rows->email; ?>">
+                                <input type="submit" value="editar">
+                            </form>
+                        </td>
+                        <td>
+                            <form action="delete.php" method="post">
+                                <input type="hidden" name="id_aluno" value="<?php echo $rows->id_aluno; ?>">
+                                <input type="submit" value="excluir">
+                            </form>
+                        </td>
+                    </tr>
+                <?php }
+            } ?>
+
         </tbody>
     </table>
-    <form action="form_create_aluno.php" method="post">
-        <input type="submit" value="cadastrar">
-    </form>
+    
+    <a href="form_create_aluno.php">cadastrar</a>
 
     <form action="logout.php" method="post">
         <input type="submit" value="sair">
