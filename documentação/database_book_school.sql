7f9f0d222ee1d0c9114e4f5249367ccc16e28b0c
luthiano0312 <luthiano.penha@aluno.ce.gov.br>
2025-06-12 21:32:33 -0300

    correção do tipo de dado e nova coluna editora

--- documentação/database_book_school.sql
+++ documentação/database_book_school.sql
@@ -31,9 +31,9 @@ CREATE TABLE livros (
     id_escola INT NOT NULL,
     titulo VARCHAR(255) NOT NULL,
     autor VARCHAR(255) NOT NULL,
-    ano_puplicacao DATE NOT NULL,
+    ano_puplicacao YEAR NOT NULL,
     genero VARCHAR(255) NOT NULL,
-    quantidade INT NOT NULL,
+    editora VARCHAR(255) NOT NULL,
     FOREIGN KEY (id_escola) REFERENCES escolas(id_escola)
 );
 
