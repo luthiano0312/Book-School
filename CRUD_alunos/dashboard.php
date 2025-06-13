cd9f6b24bab2679f70ba7437451ad2a3245da86c
luthiano0312 <luthiano.penha@aluno.ce.gov.br>
2025-06-13 12:00:26 -0300

    css dashboard

--- CRUD_alunos/dashboard.php
+++ CRUD_alunos/dashboard.php
@@ -7,71 +7,73 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../assets/dashboard_alunos_style.css">
 </head>
 <body>
-    <head> 
-        <div>Book school</div>   
-        <div>Gerenciamento de alunos</div>
-    </head>
+    <header id="header"> 
+        <div id="containerLogo">Book school</div>   
+        <div id="containerHeader">Gerenciamento de alunos</div>
+    </header>
 
-    <aside>
-        <a href="../CRUD_livros/dashboard_livros.php">Livros</a>
+    <div id="containerBody">
+        <aside id="sideBar">
+            <a href="../CRUD_livros/dashboard_livros.php">Livros</a>
 
-        <a href="../CRUD_alunos/dashboard.php">Alunos</a>
+            <a href="../CRUD_alunos/dashboard.php">Alunos</a>
 
-        <form action="logout.php" method="post">
-            <input type="submit" value="sair">
-        </form>
-    </aside>
-    <main>
-        <table>
-            <thead>
-                <th>id do aluno</th>
-                <th>id da escola</th>
-                <th>nome</th>
-                <th>email</th>
-            </thead>
+            <form action="logout.php" method="post">
+                <input type="submit" value="sair">
+            </form>
+        </aside>
+        <main id="main">
+            <table>
+                <thead>
+                    <th>id do aluno</th>
+                    <th>id da escola</th>
+                    <th>nome</th>
+                    <th>email</th>
+                </thead>
 
-            <tbody>
-                <?php
+                <tbody>
+                    <?php
 
-                require_once "../connection.php";
+                    require_once "../connection.php";
 
-                $id_escola = $_SESSION["id_escola"];
+                    $id_escola = $_SESSION["id_escola"];
 
-                $stmt = $conn->prepare("SELECT * FROM alunos WHERE id_escola = :id;");
-                $stmt->bindValue(":id", $id_escola);
-                if ($stmt->execute()) {
-                    while ($rows = $stmt->fetch(PDO::FETCH_OBJ)) { ?>
-                        <tr>
-                            <td><?php echo $rows->id_aluno; ?></td>
-                            <td><?php echo $rows->id_escola; ?></td>
-                            <td><?php echo $rows->nome; ?></td>
-                            <td><?php echo $rows->email; ?></td>
-                            <td>
-                                <form action="form_update_aluno.php" method="post">
-                                    <input type="hidden" name="id_aluno" value="<?php echo $rows->id_aluno; ?>">
-                                    <input type="hidden" name="nome" value="<?php echo $rows->nome; ?>">
-                                    <input type="hidden" name="email" value="<?php echo $rows->email; ?>">
-                                    <input type="submit" value="editar">
-                                </form>
-                            </td>
-                            <td>
-                                <form action="delete.php" method="post">
-                                    <input type="hidden" name="id_aluno" value="<?php echo $rows->id_aluno; ?>">
-                                    <input type="submit" value="excluir">
-                                </form>
-                            </td>
-                        </tr>
-                    <?php }
-                } ?>
+                    $stmt = $conn->prepare("SELECT * FROM alunos WHERE id_escola = :id;");
+                    $stmt->bindValue(":id", $id_escola);
+                    if ($stmt->execute()) {
+                        while ($rows = $stmt->fetch(PDO::FETCH_OBJ)) { ?>
+                            <tr>
+                                <td><?php echo $rows->id_aluno; ?></td>
+                                <td><?php echo $rows->id_escola; ?></td>
+                                <td><?php echo $rows->nome; ?></td>
+                                <td><?php echo $rows->email; ?></td>
+                                <td>
+                                    <form action="form_update_aluno.php" method="post">
+                                        <input type="hidden" name="id_aluno" value="<?php echo $rows->id_aluno; ?>">
+                                        <input type="hidden" name="nome" value="<?php echo $rows->nome; ?>">
+                                        <input type="hidden" name="email" value="<?php echo $rows->email; ?>">
+                                        <input type="submit" value="editar">
+                                    </form>
+                                </td>
+                                <td>
+                                    <form action="delete.php" method="post">
+                                        <input type="hidden" name="id_aluno" value="<?php echo $rows->id_aluno; ?>">
+                                        <input type="submit" value="excluir">
+                                    </form>
+                                </td>
+                            </tr>
+                        <?php }
+                    } ?>
 
-            </tbody>
-        </table>
-        
-        <a href="form_create_aluno.php">cadastrar</a>
-    </main>
-    
+                </tbody>
+            </table>
+            
+            <a href="form_create_aluno.php">cadastrar</a>
+        </main>
+    </div>
 
 </body>
 </html>
