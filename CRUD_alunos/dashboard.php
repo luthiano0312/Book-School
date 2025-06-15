dc9170e14719387fa3d8f1013a74e4862e98045e
luthiano0312 <luthiano.penha@aluno.ce.gov.br>
2025-06-15 20:24:37 -0300

    nomes das paginas

--- CRUD_alunos/dashboard.php
+++ CRUD_alunos/dashboard.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>dashboard alunos</title>
 
     <link rel="stylesheet" href="../assets/dashboard_style.css">
 
--- CRUD_livros/dashboard_livros.php
+++ CRUD_livros/dashboard_livros.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>dashboard livros</title>
     <link rel="stylesheet" href="../assets/dashboard_style.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
