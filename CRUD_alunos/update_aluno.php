a45c02dd7da22f311ae3b84ef6b20de3a2c4cf92
luthiano0312 <luthiano.penha@aluno.ce.gov.br>
2025-06-16 09:04:56 -0300

    correção bug de email ja cadastrado

--- CRUD_alunos/update_aluno.php
+++ CRUD_alunos/update_aluno.php
@@ -16,9 +16,14 @@
         $stmt = $conn->prepare("SELECT * FROM alunos WHERE email = :e");
         $stmt->bindValue(":e", $email);
         $stmt->execute();
-        $usuario = $stmt->fetch();
+        $usuarioEmail = $stmt->fetch();
 
-        if (!$usuario) {
+        $stmt = $conn->prepare("SELECT * FROM alunos WHERE id_aluno = :id");
+        $stmt->bindValue(":id", $id_aluno);
+        $stmt->execute();
+        $usuarioID = $stmt->fetch();
+
+        if (!$usuarioEmail) {
             if ($id_aluno && $email && $nome) {
                 $stmt = $conn->prepare("UPDATE alunos SET nome = :n, email = :e WHERE id_aluno = :id");
     
@@ -35,7 +40,25 @@
                 header("location: form_update_aluno.php?erro=Por favor preencha os campos&id_aluno=$id_aluno&email=$email&nome=$nome");
             }
         }else {
-            header("location: form_update_aluno.php?erro=Email já cadastrado&id_aluno=$id_aluno&email=$email&nome=$nome");
+            if ($usuarioID["email"] == $email) {
+                if ($id_aluno && $email && $nome) {
+                    $stmt = $conn->prepare("UPDATE alunos SET nome = :n, email = :e WHERE id_aluno = :id");
+        
+                    $stmt->bindValue(":n", $nome);
+                    $stmt->bindValue(":e", $email);
+                    $stmt->bindValue(":id", $id_aluno);
+            
+                    if($stmt->execute()) {
+                        header("location: dashboard.php?sucesso=Atualizado com sucesso");
+                    } else {
+                        header("location: form_update_aluno.php?erro=Erro na atualização");
+                    }
+                } else {
+                    header("location: form_update_aluno.php?erro=Por favor preencha os campos&id_aluno=$id_aluno&email=$email&nome=$nome");
+                }
+            }else {
+                header("location: form_update_aluno.php?erro=Email já cadastrado&id_aluno=$id_aluno&email=$email&nome=$nome");
+            }
         }
     ?>
 
